084ca90136af90e7738af2ee3e4a72fb4913fa3a
ReginPotter <reginpotter@pop-os.localdomain>
2023-02-05 12:08:45 -0500

    Made script human readable

--- xmlCreation.py
+++ xmlCreation.py
@@ -7,12 +7,13 @@ input = input()
 gtin = re.search(r"F7[A-Za-z0-9]{15}", input)
 gtin = gtin.group(0)
 gtin = gtin[3:]
+print("GTIN: " + gtin)
 
 date = str(datetime.now())
 date = date[:-3]
 date = date.replace(' ', 'T')
-print(date)
-date += '-05:00'
+print("DATE: " + date)
+
 
 # We can change the xml file however in order to do that we need to create a new file.
 # Given the time we have left we cannot make this change
