24506272e0c5409777b754bca3540e8b43997e38
Anmol Saini <anmolsaini@pop-os.localdomain>
2023-02-05 12:01:42 -0500

    Clean up

--- post-request.py
+++ post-request.py
@@ -1,7 +1,5 @@
 import requests
 
 url = 'https://freepcis.gs1.org/server/Hackathon/capture'
-print(url)
 eventfile = open('./event.xml')
-
-x = requests.post(url, data = eventfile)
+requests.post(url, data = eventfile)
